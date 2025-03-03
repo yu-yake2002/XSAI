998fa2e533fd446244406800187aae1c7f766d8e
yu-yake2002 <yuyake02@outlook.com>
2025-03-03 16:54:19 +0800

    fix(matrix): Fix bugs in mset insts
    - Remove unused read ports in MSET ExeUnits. FuConfig is updated as
      well to reflect the port removals.
    - Use separate mtilem/n/k_idx instead of mtilex_idx.
    - Add operands for csr mlenb/mrlenb/mamul in decode stage.
    - Prune mstart/mtype I/O from DecodeStage, DecodeUnit, DecodeUnitComp.

--- src/main/scala/xiangshan/backend/rob/Rob.scala
+++ src/main/scala/xiangshan/backend/rob/Rob.scala
@@ -1726,6 +1726,8 @@ class RobImp(override val wrapper: Rob)(implicit p: Parameters, params: BackendP
     dontTouch(io.commits)
     dontTouch(commitIsVTypeVec)
     dontTouch(walkIsVTypeVec)
+    dontTouch(commitIsMTypeVec)
+    dontTouch(walkIsMTypeVec)
     dontTouch(commitValidThisLine)
     dontTouch(commitReadAddr_next)
     dontTouch(donotNeedWalk)
